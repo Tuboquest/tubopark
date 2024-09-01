8f495cd655697389d43438246e0fd9473b3d74b4
Florian <florian.wu@epitech.eu>
2024-09-01 04:19:57 +0200

    feat : statistics from last 6 months

--- app/(profil)/statistics.tsx
+++ app/(profil)/statistics.tsx
@@ -1,54 +1,59 @@
 import { Stats } from "@/api/statistics";
 import { GradientBackground } from "@/components/GradientBackground";
 import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-  Dimensions,
-} from "react-native";
-
-import {
-  LineChart,
-} from "react-native-chart-kit";
+import { View, Text, StyleSheet } from "react-native";
+import { LineChart } from "react-native-chart-kit";
 
 export default function StatisticScreen() {
-
-  const [statisticsState,
-    setStatisticsState] = useState<any>();
-  const data = {
-    labels: [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ],
-    datasets: [
-      {
-        data: [20, 45, 28, 80, 99, 43],
-      },
-    ],
-  };
+  const [statisticsState, setStatisticsState] = useState<any>();
 
   useEffect(() => {
     const fetchData = async () => {
       const tampData = await Stats.getStatistics();
-      setStatisticsState([...tampData]);
+      setStatisticsState({ ...tampData });
     };
     fetchData();
   }, []);
 
-  console.log("stats : ", statisticsState);
+  const getLastSixMonths = (data: any) => {
+    const allMonths = [
+      "Janvier",
+      "Février",
+      "Mars",
+      "Avril",
+      "Mai",
+      "Juin",
+      "Juillet",
+      "Août",
+      "Septembre",
+      "Octobre",
+      "Novembre",
+      "Décembre",
+    ];
+
+    const currentMonthIndex = new Date().getMonth(); // Get the current month index (0-11)
+    const lastSixMonths = [];
+    const lastSixValues = [];
+
+    // Iterate backwards from the current month to get the last six months
+    for (let i = 5; i >= 0; i--) {
+      const monthIndex = (currentMonthIndex - i + 12) % 12; // Calculate month index accounting for wrap-around
+      const monthName = allMonths[monthIndex];
+      lastSixMonths.push(monthName);
+      lastSixValues.push(data[monthName] || 0); // Use 0 if the month doesn't exist in the data
+    }
+
+    return {
+      labels: lastSixMonths,
+      datasets: [
+        {
+          data: lastSixValues,
+        },
+      ],
+    };
+  };
+
+  const filteredData = statisticsState ? getLastSixMonths(statisticsState) : null;
 
   return (
     <GradientBackground>
@@ -56,45 +61,39 @@ export default function StatisticScreen() {
         <View style={styles.titleContainer}>
           <Text style={styles.title}>Your Statistics</Text>
         </View>
-        <Text style={styles.title}>number of times used</Text>
-        {statisticsState ? <LineChart
-          data={{
-            labels: ["January", "February", "March", "April", "May", "June"],
-            datasets: [
-              {
-                data: [5, 0, 6, 10, 15, 6],
+        <Text style={styles.title}>Disk rotation</Text>
+        {filteredData ? (
+          <LineChart
+            data={filteredData}
+            width={350}
+            height={220}
+            yAxisSuffix=""
+            yAxisInterval={1}
+            chartConfig={{
+              backgroundColor: "#074264",
+              backgroundGradientFrom: "#074264",
+              backgroundGradientTo: "#83A5C7",
+              decimalPlaces: 2,
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16,
               },
-            ],
-          }}
-          width={350} // from react-native
-          height={220}
-          // yAxisLabel="day"
-          yAxisSuffix=""
-          yAxisInterval={1} // optional, defaults to 1
-          chartConfig={{
-            backgroundColor: "#074264",
-            backgroundGradientFrom: "#074264",
-            backgroundGradientTo: "#83A5C7",
-            decimalPlaces: 2, // optional, defaults to 2dp
-            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            style: {
+              propsForDots: {
+                r: "6",
+                strokeWidth: "2",
+                stroke: "#ffa726",
+              },
+            }}
+            bezier
+            style={{
+              marginVertical: 8,
               borderRadius: 16,
-            },
-            propsForDots: {
-              r: "6",
-              strokeWidth: "2",
-              stroke: "#ffa726",
-            },
-          }}
-          bezier
-          style={{
-            marginVertical: 8,
-            borderRadius: 16,
-          }}
-        /> :
-        <></>
-        }
+            }}
+          />
+        ) : (
+          <Text style={styles.title}>Loading...</Text>
+        )}
       </View>
     </GradientBackground>
   );
@@ -105,7 +104,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     paddingHorizontal: 20,
-    // marginTop: 250,
   },
   titleContainer: {
     width: "100%",
